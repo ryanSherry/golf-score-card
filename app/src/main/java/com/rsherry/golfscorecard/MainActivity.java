bd80aba10281ae6c6d8da1c533bd4af4ca98e37d
Ryan Sherry <rsherrycomputers@gmail.com>
2018-04-16 22:59:52 -0700

    added total sum of swings

--- app/src/main/java/com/rsherry/golfscorecard/MainActivity.java
+++ app/src/main/java/com/rsherry/golfscorecard/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView sumOfSwingsLabel;
+    int sumOfSwings = 0;
+
     Button hole1Minus;
     Button hole1Plus;
     TextView hole1Label;
@@ -120,6 +123,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static  final String KEY_HOLE_16 =  "KEY_HOLE_16";
     private static  final String KEY_HOLE_17 =  "KEY_HOLE_17";
     private static  final String KEY_HOLE_18 =  "KEY_HOLE_18";
+    private static final String KEY_SWING_TOTAL = "KEY_SWING_TOTAL";
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -143,6 +147,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         outState.putInt(KEY_HOLE_16,hole16Swings);
         outState.putInt(KEY_HOLE_17,hole17Swings);
         outState.putInt(KEY_HOLE_18,hole18Swings);
+        outState.putInt(KEY_SWING_TOTAL,sumOfSwings);
     }
 
     @Override
@@ -166,6 +171,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hole16Swings = savedInstanceState.getInt(KEY_HOLE_16);
         hole17Swings = savedInstanceState.getInt(KEY_HOLE_17);
         hole18Swings = savedInstanceState.getInt(KEY_HOLE_18);
+        sumOfSwings = savedInstanceState.getInt(KEY_SWING_TOTAL);
+
 
         hole1Label.setText(String.valueOf(hole1Swings));
         hole2Label.setText(String.valueOf(hole2Swings));
@@ -185,6 +192,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hole16Label.setText(String.valueOf(hole16Swings));
         hole17Label.setText(String.valueOf(hole17Swings));
         hole18Label.setText(String.valueOf(hole18Swings));
+        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
     }
 
     @Override
@@ -266,6 +274,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hole18Plus = findViewById(R.id.hole18Plus);
         hole18Label = findViewById(R.id.hole18Label);
 
+        sumOfSwingsLabel = findViewById(R.id.sumOfSwingsLabel);
+
 
         //call the onClick method on buttons
         hole1Minus.setOnClickListener(this);
@@ -332,181 +342,253 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     case R.id.hole1Minus:
                         if(hole1Swings != 0) hole1Swings--;
                         hole1Label.setText(String.valueOf(hole1Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole1Plus:
                         hole1Swings++;
                         hole1Label.setText(String.valueOf(hole1Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole2Minus:
                         if(hole2Swings != 0) hole2Swings--;
                         hole2Label.setText(String.valueOf(hole2Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole2Plus:
                         hole2Swings++;
                         hole2Label.setText(String.valueOf(hole2Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole3Minus:
                         if(hole3Swings != 0) hole3Swings--;
                         hole3Label.setText(String.valueOf(hole3Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole3Plus:
                         hole3Swings++;
                         hole3Label.setText(String.valueOf(hole3Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole4Minus:
                         if(hole4Swings != 0) hole4Swings--;
                         hole4Label.setText(String.valueOf(hole4Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole4Plus:
                         hole4Swings++;
                         hole4Label.setText(String.valueOf(hole4Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole5Minus:
                         if(hole5Swings != 0) hole5Swings--;
                         hole5Label.setText(String.valueOf(hole5Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole5Plus:
                         hole5Swings++;
                         hole5Label.setText(String.valueOf(hole5Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole6Minus:
                         if(hole6Swings != 0) hole6Swings--;
                         hole6Label.setText(String.valueOf(hole6Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole6Plus:
                         hole6Swings++;
                         hole6Label.setText(String.valueOf(hole6Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole7Minus:
                         if(hole7Swings != 0) hole7Swings--;
                         hole7Label.setText(String.valueOf(hole7Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole7Plus:
                         hole7Swings++;
                         hole7Label.setText(String.valueOf(hole7Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole8Minus:
                         if(hole8Swings != 0) hole8Swings--;
                         hole8Label.setText(String.valueOf(hole8Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole8Plus:
                         hole8Swings++;
                         hole8Label.setText(String.valueOf(hole8Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole9Minus:
                         if(hole9Swings != 0) hole9Swings--;
                         hole9Label.setText(String.valueOf(hole9Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole9Plus:
                         hole9Swings++;
                         hole9Label.setText(String.valueOf(hole9Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole10Minus:
                         if(hole10Swings != 0) hole10Swings--;
                         hole10Label.setText(String.valueOf(hole10Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole10Plus:
                         hole10Swings++;
                         hole10Label.setText(String.valueOf(hole10Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole11Minus:
                         if(hole11Swings != 0) hole11Swings--;
                         hole11Label.setText(String.valueOf(hole11Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole11Plus:
                         hole11Swings++;
                         hole11Label.setText(String.valueOf(hole11Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole12Minus:
                         if(hole12Swings != 0) hole12Swings--;
                         hole12Label.setText(String.valueOf(hole12Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole12Plus:
                         hole12Swings++;
                         hole12Label.setText(String.valueOf(hole12Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole13Minus:
                         if(hole13Swings != 0) hole13Swings--;
                         hole13Label.setText(String.valueOf(hole13Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole13Plus:
                         hole13Swings++;
                         hole13Label.setText(String.valueOf(hole13Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole14Minus:
                         if(hole14Swings != 0) hole14Swings--;
                         hole14Label.setText(String.valueOf(hole14Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole14Plus:
                         hole14Swings++;
                         hole14Label.setText(String.valueOf(hole14Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole15Minus:
                         if(hole15Swings != 0) hole15Swings--;
                         hole15Label.setText(String.valueOf(hole15Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole15Plus:
                         hole15Swings++;
                         hole15Label.setText(String.valueOf(hole15Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole16Minus:
                         if(hole16Swings != 0) hole16Swings--;
                         hole16Label.setText(String.valueOf(hole16Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole16Plus:
                         hole16Swings++;
                         hole16Label.setText(String.valueOf(hole16Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole17Minus:
                         if(hole17Swings != 0) hole17Swings--;
                         hole17Label.setText(String.valueOf(hole17Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole17Plus:
                         hole17Swings++;
                         hole17Label.setText(String.valueOf(hole17Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole18Minus:
                         if(hole18Swings != 0) hole18Swings--;
                         hole18Label.setText(String.valueOf(hole18Swings));
+                        if (sumOfSwings != 0) sumOfSwings--;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
                     case R.id.hole18Plus:
                         hole18Swings++;
                         hole18Label.setText(String.valueOf(hole18Swings));
+                        sumOfSwings++;
+                        sumOfSwingsLabel.setText(String.valueOf(sumOfSwings));
                         break;
 
 
